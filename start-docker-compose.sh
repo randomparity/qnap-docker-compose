ac3486116b5c5f285ca32ff3d74f02f6fb47409b
David Christensen <randomparity@gmail.com>
2023-11-05 08:05:28 -0800

    Update start/stop scripts for new docker compose command

--- start-docker-compose.sh
+++ start-docker-compose.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-$(getcfg container-station Install_path -f /etc/config/qpkg.conf)/bin/docker-compose -f /share/Container/src/qnap-docker-compose/docker-compose.yml up -d --remove-orphans
+# $(getcfg container-station Install_path -f /etc/config/qpkg.conf)/bin/docker-compose -f /share/Container/src/qnap-docker-compose/docker-compose.yml up -d --remove-orphans
+docker compose -f /share/Container/src/qnap-docker-compose/docker-compose.yml up -d --remove-orphans
--- stop-docker-compose.sh
+++ stop-docker-compose.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-$(getcfg container-station Install_path -f /etc/config/qpkg.conf)/bin/docker-compose -f /share/Container/src/qnap-docker-compose/docker-compose.yml stop
+# $(getcfg container-station Install_path -f /etc/config/qpkg.conf)/bin/docker-compose -f /share/Container/src/qnap-docker-compose/docker-compose.yml stop
+docker compose -f /share/Container/src/qnap-docker-compose/docker-compose.yml stop
